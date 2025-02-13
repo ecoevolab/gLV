f12349b563c68d01688b59bcfb8abfc19fd8a6e1
Manuel <mrivera@chromatin.cm.cluster>
2025-02-13 09:24:24 -0600

    Added HeatMap proportions

--- HeatMap-params.R
+++ HeatMap-params.R
@@ -1,6 +1,6 @@
 
 #------------Load master table#------------#
-data <- read.delim("/mnt/atgc-d3/sur/users/mrivera/testing/Sims_Exp02/NA-counts-exp02.tsv", sep = "\t", header = TRUE)
+data <- read.delim("/mnt/atgc-d3/sur/users/mrivera/glv-research/Results/Unified-exp01/NA-PropsUnified-D10M02Y24.tsv", sep = "\t", header = TRUE)
 
 params_table <- read.delim("/mnt/atgc-d3/sur/users/mrivera/glv-research/Data/Params-exp01.tsv", sep = "\t", header = TRUE)
 
@@ -31,25 +31,29 @@ df_summed <- df_joined |>
   group_by(Prob_neg, Prob_0) |> 
   summarise(Total_NA_counts = sum(`NA-counts`, na.rm = TRUE))
 
+# Count the ocurrances per group
+counts_df <-  df_joined %>% count(Prob_neg, Prob_0, sort = TRUE,  name = "num_counts")
+
+# Join the count dataframe back to df_joined
+df_result <- df_summed %>% left_join(counts_df, by = c("Prob_neg", "Prob_0")) %>%
+  mutate(adjusted_NA = Total_NA_counts / num_counts)
+
 # Create HeatMap
 library(tidyr)
 library(ggplot2)
 library(plotly)
 
-p <- ggplot(df_summed, aes(x = Prob_neg, y = Prob_0, fill = `Total_NA_counts`)) +
+p <- ggplot(df_result, aes(x = Prob_neg, y = Prob_0, fill = `adjusted_NA`)) +
   geom_tile() +
   scale_fill_distiller(palette = "RdBu", direction = 1) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  labs(title = "Heatmap of NA Counts", x = "Negative Probability", y = "No Interaction Probability", fill = "NAs") +
-  annotation_custom(segmentsGrob(c(0.3, -0.1), c(-0.085, 0.28), 
-                                 c(1, -0.1), c(-0.085, 1), gp = gpar(lwd = 2),
-                                 arrow = arrow(length = unit(2.5, 'mm'))))
+  labs(title = "Heatmap of NA Counts", x = "Negative Probability", y = "No Interaction Probability", fill = "NAs")
 
 # Convert ggplot to an interactive plot
 interactive_p <- ggplotly(p)
 
 # Save as an interactive HTML file
 library(htmlwidgets)
-saveWidget(interactive_p, "/mnt/atgc-d3/sur/users/mrivera/glv-research/Graphs/HeatMap_exp02-old.html", selfcontained = FALSE)
+saveWidget(interactive_p, "/mnt/atgc-d3/sur/users/mrivera/glv-research/Graphs/PropsHeatMap-Exp01-Params.html", selfcontained = FALSE)
 
