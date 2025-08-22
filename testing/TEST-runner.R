c30f2f6f2c9b001bff768c290ca73febc19487a7
Manuel <mrivera@protein.cm.cluster>
2025-08-22 14:32:01 -0600

    Final push SGE cluster

--- testing/TEST-runner.R
+++ testing/TEST-runner.R
@@ -1,7 +1,7 @@
 #' ---
-#' title: "ExpM279 notebook"
+#' title: "Testing notebook"
 #' author: "Manuel Rivera"
-#' date: "2025-03-04"
+#' date: "2025-05-20"
 #' output: 
 #'   html_document:
 #'   toc: true           # Enable Table of Contents
@@ -16,10 +16,8 @@
 BASE_DIR <- "/mnt/atgc-d3/sur/users/mrivera/glv-research"
 DATA_DIR <- file.path(BASE_DIR, "Data")
 RESULTS_DIR <- file.path(BASE_DIR, "Results")
-exp_id <- paste0("Exp06-D", format(Sys.Date(), "%d-%b"))
-#====================================================================
-# Testing line
-exp_id <- "Exp06-D29-Apr-R02"
+exp_id <- paste0("TEST01-D", format(Sys.Date(), "%d-%B"))
+
 #========================================================
 exp_dir <- file.path(RESULTS_DIR, exp_id) # Experiment directory
 params_path <- file.path(DATA_DIR, paste0(exp_id, ".tsv")) # Parameters TSV
@@ -37,45 +35,45 @@ tictoc::tic("Section 1: Time for Parameter Generation")
 
 #' Generate grid of parameters:
 #+ eval=FALSE
-generate_params <- function (){
-  dt <- data.table::CJ(n_species = rep(c(20, 100), times = 300), p_neg = 1, p_noint = seq(0, 1, by = 0.05))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(data.table))
 
-  # Add Columns with data table operator `:=`
-  dt[, `:=`(
-    id = ids::random_id(n = .N, bytes = 5),
-    x0_seed = sample(1:1e6, .N, replace = FALSE),
-    mu_seed = sample(1:1e6, .N, replace = FALSE),
-    A_seed = sample(1:1e6, .N, replace = FALSE)
-  )]
+generate_params <- function (){
+ dt <- tidyr::expand_grid(
+    n_species = seq(20, 100, by = 20),
+    p_neg = seq(0, 1, by = 0.1), 
+    p_noint = seq(0, 1, by = 0.1)
+  ) %>%
+  dplyr::filter(!(p_noint == 0 & p_neg == 0)) %>% # Exclude where p_noint=p_neg = 0
+  tidyr::uncount(weights = 10) %>%
+  dplyr::mutate(
+    id = ids::random_id(n = dplyr::n(), bytes = 5),
+    x0_seed = sample(1:1e6, dplyr::n(), replace = FALSE),
+    mu_seed = sample(1:1e6, dplyr::n(), replace = FALSE),
+    A_seed = sample(1:1e6, dplyr::n(), replace = FALSE)
+  )
   
   return(dt)
 }
-#====================================================================
-# Testing line
-params_path <- "/mnt/atgc-d3/sur/users/mrivera/glv-research/Data/Exp06-D29-Apr.tsv"
+
 #========================================================
-# Save Parameters as TSV or load them
-if (!(file.exists(params_path))) {
 
-  # Generate parameters
-  params_df <- generate_params()
+# Generate parameters
+params_df <- generate_params()
 
-  # Verify if ids are unique and in case they are, save the parameters.
-  while (nrow(params_df) != length(unique(params_df$id))) {
-    params_df <- generate_params() # Repeat function
-  }
-
-  # Save them
-  data.table::fwrite(x = params_df, file = params_path, 
-    sep = "\t",
-    quote = FALSE,      # Disable quoting for faster writing
-    row.names = FALSE,  # Don't write row names
-  )
-  message("\nParameteres generated and saved at path:\n", params_path, "\n")
-} else {
-  params_df <- data.table::fread(params_path)
-  message("\nParameters loaded..\n", params_path)
+# Verify if ids are unique and in case they are, save the parameters.
+while (nrow(params_df) != length(unique(params_df$id))) {
+  params_df <- generate_params() # Repeat function
 }
+
+# Save them
+data.table::fwrite(x = params_df, file = params_path, 
+  sep = "\t",
+  quote = FALSE,      # Disable quoting for faster writing
+  row.names = FALSE,  # Don't write row names
+)
+message("\nParameteres generated and saved at path:\n", params_path, "\n")
+
 tictoc::toc() # For section 1
 
 #' We split the data into chunks of `n_cores`:
@@ -83,21 +81,27 @@ tictoc::toc() # For section 1
 # ==== Split data into chunks====
 tictoc::tic("Section 2: Divide data into chunks")
 library(parallel)
-num_cores <- parallel::detectCores() - 1  # Use one less than the total number of cores
+num_cores <- 60  # Use one less than the total number of cores
+
+cat(rep("=", 40), "\n")
 cat("The number of cores that will be used are: ", num_cores, "\n")
+cat("Each core will do: ", nrow(params_df)/num_cores, " simulations\n")
+cat(rep("=", 40), "\n")
 
 split_table <- function(df, n_chunks) {
   split(df, cut(seq_len(nrow(df)), breaks = n_chunks, labels = FALSE))
 }
 
+chunks <- split_table(params_df, num_cores)
+message("\nData split completed...\n")
+
+tictoc::toc() # For section 2
 #====================================================================
 # Testing line
 # num_cores <- 5
 # chunks <- split_table(params_df[1:10], num_cores)
 #====================================================================
-chunks <- split_table(params_df, num_cores)
-message("\nData split completed...\n")
-tictoc::toc() # For section 2
+
 
 #' We create a separate directory for each core to prevent race conditions (when two cores access the same directory simultaneously).
 #+ eval=FALSE
@@ -123,70 +127,55 @@ tictoc::toc() # For section 3
 
 #' We source the function to generate the gLV parameters using the initial parameters and for solve it:
 
+
+#===================================================
+# Testing lines
+# index = params_df[1,]
+# times=500
+# params = regenerate(index)
+#===========================
+
 #+ eval=FALSE
 # Source functions to:
-source("/mnt/atgc-d3/sur/users/mrivera/glv-research/GIT-gLV/src/generate-params.R") # regenerate parameters
-source("/mnt/atgc-d3/sur/users/mrivera/glv-research/GIT-gLV/src/solver-gLV.R") # solve gLV equations
-source("/mnt/atgc-d3/sur/users/mrivera/glv-research/GIT-gLV/src/forge-symls.R") # generate symbolic links
-source("/mnt/atgc-d3/sur/users/mrivera/glv-research/GIT-gLV/src/find-time-stability.R") # source function for ts
-source("/mnt/atgc-d3/sur/users/mrivera/glv-research/GIT-gLV/src/extinctions-fun.R") # Extinction function
+source("/mnt/atgc-d3/sur/users/mrivera/glv-research/GIT-gLV/testing/generate-params.R") # regenerate parameters
+source("/mnt/atgc-d3/sur/users/mrivera/glv-research/GIT-gLV/testing/solver-gLV.R") # solve gLV equations
 
 #' We wrap the code for parallelizing the simulations.
 #+ eval=FALSE
 # ==== Wrapper for running all required steps ====
 sims_wrapper <- function(index, path_core) {
   params <- regenerate(index) # Generate parameters
-  output <- solve_gLV(times = 1000, params) # Run simulation
+  output <- solve_gLV(times = 500, params) # Run simulation
 
   # Define paths
   path_ode <- file.path(path_core, paste0("O_", params$id, ".feather")) # Output ODE
-  ext_path <-  file.path(path_core, paste0("E_", params$id, "-Info.feather")) # Extinctions
-
-  NA_count <- sum(is.na(output)) # Calculate NAs
-  ts_out <- find_ts(output) # Find time-to-stability (ts) OUTPUT
+  NA_count <- as.integer(any(is.na(output))) # Calculate NAs
   arrow::write_feather(output, path_ode) # Save simulation
 
-  #=========== Simulate extinctions ================
-  ext_list <- sim_all_ext(output, params) # Generate extinctions
-
-  if (is.null(ext_list)) {
-    cat(">> Simulation", params$id, "extinctions not performed, low survivors...\n", sep = " ")
-    cat(">> Simulation", params$id, "completed...\n", sep = " ")
-    return(list(id = params$id, na_ct = NA_count, ts_out = ts_out, ts_ext = 0, n_ext = 0))
-  }
-
-  ts_ext <- max(ext_list$info[["ext_ts"]]) # Find ts on EXTINCTIONS
-  n_ext <- ext_list$n_ext # Number of extinctions
-
-  # Save EXTINCTIONS output per specie
-  tmp <- names(ext_list$data)
-  ext_paths <- paste0(path_core, "/E_", params$id, "-", tmp, ".feather") 
-  lapply(seq_along(ext_list$data), function(e) {
-    arrow::write_feather(ext_list$data[[e]], ext_paths[e])
-  })
-
-  # Save EXTINCTIONS info
-  arrow::write_feather(ext_list$info, ext_path) 
-
   cat(">> Simulation", params$id, "completed...\n", sep = " ")
-  return(list(id = params$id, na_ct = NA_count, ts_out = ts_out, ts_ext = ts_ext, n_ext = n_ext ))
+  return(list(id = params$id, na_ct = NA_count))
 }
 
 #' We Parallelize the code and get the summary of simulations
 #+ eval=FALSE
 # ==== Parallelize it ====
-tictoc::tic("Section 4: Run simulations and extinctions using the parallel package")
+tictoc::tic("Section 4: Run simulations using the parallel package")
 
+core_id = 1
 sims_info <- parallel::mclapply(1:num_cores, function(core_id) {
   
   message("Starting worker ", core_id, "....\n")
   
   core_chunk <- chunks[[core_id]]  # rows assigned to this core
   
+  start_time <- Sys.time()  # Start timing
   result <- lapply(1:nrow(core_chunk), function(i) {
     sims_wrapper(index = core_chunk[i, ], path_core = workers_ODE[core_id])
-    
   })
+
+  end_time <- Sys.time()  # End timing
+  elapsed <- difftime(end_time, start_time, units = "secs")
+  message("Worker ", core_id, " finished in ", round(elapsed, 2), " seconds.\n")
 
   result <- data.table::rbindlist(result, use.names = TRUE) # Convert list to df
   message("Ending worker ", core_id, "....\n")
@@ -200,10 +189,4 @@ sims_info_df <- data.table::rbindlist(sims_info) # Convert list (of df) to df
 data.table::fwrite(x = sims_info_df, file = info_path, sep = "\t")
 tictoc::toc() # For section 4
 
-#' We create symbolic links of the simulation...
-#+ eval=FALSE
-# ==== Create symbolic links====
-tictoc::tic("Section 5: Generate symbolic links")
-purrr::walk(workers_ODE, ~gen_syml(.x, exp_dir))
-tictoc::toc() # For section 5
 tictoc::toc() # For Total running time
