96855b2107ec25cc5a0ab86c26938bb8953a63ee
Rivera <rivera20c3@gmail.com>
2026-01-24 20:30:25 -0600

    Added Concat=False to GATCONV. We are looking for the average of the outputs, not the concatenation of them

--- GNN/src/Model-GATConv.py
+++ GNN/src/Model-GATConv.py
@@ -4,17 +4,17 @@ import torch.nn.functional as F
 from torch_geometric.nn import GATConv
 
 class ModelGATConv(nn.Module):
-    def __init__(self, hidden_channels=64, num_layers=5, dropout=0.5, h = 1):
+    def __init__(self, hidden_channels=64, num_layers=5, dropout=0.5, h = 1, concat=False):
         super().__init__()
         self.convs = nn.ModuleList()
         self.dropout = dropout
         # First layer: 1 -> hidden_channels
-        self.convs.append(GATConv(in_channels = 1, out_channels = hidden_channels, heads = h))
+        self.convs.append(GATConv(in_channels = 1, out_channels = hidden_channels, heads = h, concat=concat))
         # Middle layers: hidden_channels -> hidden_channels
         for _ in range(num_layers - 2):
-            self.convs.append(GATConv(in_channels = hidden_channels, out_channels = hidden_channels, heads = h))
+            self.convs.append(GATConv(in_channels = hidden_channels, out_channels = hidden_channels, heads = h, concat=concat))
         # Last layer: hidden_channels -> 1
-        self.convs.append(GATConv(in_channels = hidden_channels, out_channels = 1, heads = h))
+        self.convs.append(GATConv(in_channels = hidden_channels, out_channels = 1, heads = h, concat=concat))
     def forward(self, data):
         x, edge_index, edge_weight = data.x, data.edge_index, data.edge_weights
         # Apply all layers except the last
