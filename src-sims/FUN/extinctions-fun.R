91998ad2092d95d057edf77d48689e05f5226d7c
Rivera <rivera20c3@gmail.com>
2026-02-09 22:08:49 -0600

    changeed extinction functions to remove species from the community for quantifying it is impact

--- src-sims/FUN/extinctions-fun.R
+++ src-sims/FUN/extinctions-fun.R
@@ -29,45 +29,64 @@
 #'
 
 sim_all_ext <- function(params, path_core) {
-  
-  # Function to simulate extinction for a single species
-  x = params$x0
-  df = data.frame()
+  # Extract parameters
   n = params$n
-  for(i in seq_along(x)){
-    x_new = x
-    x_new[i] = 0                # extinct-specie i
-    params$x0 = x_new           # update params
-    new_out = solve_gLV(times = 1000, params)
-    x_end = new_out[,1000]
-    #--------------------Bray-Curtis-------------------------#
-    bray_curtis <- 1 - (2 * sum(pmin(x, x_end))) / (sum(x) + sum(x_end))
-    #--------------------Extinctions-------------------------#
-    # All that was live before (x > 1e-06)
-    # But now is dead (x <= 1e-06)
-    new_ext <- sum(x_end <= 1e-6 & x > 1e-6)  # raw number of extinctions
-    props_ext = new_ext/n                     # proportion of extinctions
-    #--------------------Keystoneness-------------------------#
-    props <- x_end / sum(x_end) # relative abundance
-    K_s <- bray_curtis * (1 - props[i])
+  x_before = params$x0                              # Non perturbated populations  
+  rel_pop_initial <- x_before / sum(x_before)       # Proportions
+  # Pre-allocate data frame
+  exts_df <- data.frame(
+    specie = integer(n),
+    n_extinctions = integer(n),
+    prop_extinctions = numeric(n),
+    dissimilarity_bc = numeric(n),
+    keystoneness = numeric(n),
+    time_stability = numeric(n)
+  )
+  for(i in 1:n){
+    # Remove species i from the community
+    tmp_params <- list(
+      x0 = params$x0[-i],
+      mu = params$mu[-i],
+      M = params$M[-i, -i, drop = FALSE]
+    )
+    #------------------------------------
+    # Run simulation
+    new_out = solve_gLV(times = 1000, tmp_params)
+    x_after = new_out[, ncol(new_out)]                 # Last column 
+    #------------------------------------
+    # Section: Extinctions
+    extinct_after = x_after <= 1e-6                   # NOW DIED
+    extinct_before = tmp_params$x0 > 1e-6                  # WERE ALIVE
+    n_extinctions <- sum(extinct_after & extinct_before)    # new extinctions
+    props_extinctions = n_extinctions/n                     # proportion of extinctions
+    #------------------------------------
+    # Section: Bray-Curtis dissimilarity 
+    # Remove species i from the original community.
+    # Calculate
+    x_removed = x_before[-i]
+    bray_curtis <- 1 - (2 * sum(pmin(x_removed, x_after))) / (sum(x_removed) + sum(x_after))
+    #------------------------------------
+    # Section: Keystoneness 
+    props <- x_after / sum(x_after) # relative abundance
+    keystoneness <- bray_curtis * (1 - props[i])
     #--------------------Time to stability-------------------------#
-    ext_ts <- find_ts(new_out)
+    time_stability <- find_ts(new_out)
     # 
     # Generate data frame
-    row <- data.frame(
-      spec = i,                     # specie-extinct
-      new_ext = new_ext,            # new-extinctions
-      props_ext = round(props_ext, 2), # proportion-extinctions
-      BC_diss = bray_curtis,        # Bray-Curtis
-      K_s = K_s,                    # Keystoness
-      ext_ts = ext_ts               # Time-to-stability
-    )
-    df = rbind(df, row)
-    ext_path <- paste0(path_core, "/E_", params$id, "-S", i, ".feather")          # Extinctions-paths
-    arrow::write_feather(x = new_out, sink = ext_path)                                       # Save-extinctions
+    exts_df[i, "specie"] <- i                                     # specie-extinct
+    exts_df[i, "n_extinctions"] <- n_extinctions                  # new-extinctions
+    exts_df[i, "prop_extinctions"] <- round(props_extinctions, 2) # proportion-extinctions
+    exts_df[i, "dissimilarity_bc"] <- bray_curtis                 # Bray-Curtis
+    exts_df[i, "keystoneness"] <- keystoneness                    # Keystoneness             
+    exts_df[i, "time_stability"] <- time_stability                # Time-to-stability          
+    # Lines to save the output of each extinction 
+    # ext_path <- paste0(path_core, "/E_", params$id, "-S", i, ".feather")         
+    # arrow::write_feather(x = new_out, sink = ext_path)                                       
   }
+  # Add relative abundance of the extinct species before extinction
+  exts_df$rel_pop_initial <- rel_pop_initial   
   cat(">> Extinctions completed for", params$id, ".\n")
-  return(df)
+  return(exts_df)
 }
 
 
