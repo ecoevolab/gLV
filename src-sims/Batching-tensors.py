90d97fcf15ff0937dc68b943b984c11e2b1dde7c
Manuel Rivera Ceron <mrivera@login01.cluster>
2026-02-20 14:50:16 -0600

    modified code to create tensors to include topology and dummy data

--- src-sims/Batching-tensors.py
+++ src-sims/Batching-tensors.py
@@ -1,4 +1,9 @@
+# This code is done for creating the batces of tensors required for training the GNN. 
+# It is done in a parallelized way, and the batches are saved in a directory as .pt files. 
+# Finally, the entire directory is zipped.
 
+#-------------------------------------------------------------
+# Section: Import data
 # Load data
 import pandas as pd
 import numpy as np
@@ -7,11 +12,11 @@ import os
 from datetime import datetime
 
 # Section: Generate-paths
-exp_dir = "/mnt/data/sur/users/mrivera/Controls/exp_20251125"
-A_dir = os.path.join(exp_dir, "A-mat")
-tgt_dir = os.path.join(exp_dir, "GNN-targets")
-odes_path = os.path.join(exp_dir, "raw-ODEs")
-data_path = os.path.join(exp_dir, "Simulation-parameters.tsv")
+experiment_dir = "/mnt/data/sur/users/mrivera/Controls/NegCtrl-V1"
+interactions_dir = os.path.join(experiment_dir, "Interactions")
+targets_dir = os.path.join(experiment_dir, "ExtSummaries")
+topologies_dir = os.path.join(experiment_dir, "Topologies")
+data_path = os.path.join(experiment_dir, "simulation-params.tsv")
 
 # Generate ID for training.
 timeID = datetime.now().strftime("Y%YM%mD%d")
@@ -19,33 +24,46 @@ timeID = datetime.now().strftime("Y%YM%mD%d")
 #  Load-data
 data_ids = pd.read_csv(data_path, sep="\t", usecols=['id'])['id']
 
-#===============================================
-# Loading functions
-# SECTION: Load-function
+#-------------------------------------------------------------
+# SECTION: Generate function to load data for one simulation.
 from torch_geometric.data import Data
 import pyarrow.feather as feather
+id = data_ids.iloc[0]
 
-def load_single_data(id, A_dir, tgt_dir):
-    # Load adjacency matrix 
-    A_path = os.path.join(A_dir, f"A_{id}.feather")
+def load_single_data(id, interactions_dir, targets_dir, topologies_dir):
+    #------------------------------
+    # Section: Load adjacency matrix 
+    A_path = os.path.join(interactions_dir, f"A_{id}.feather")
     A = pd.read_feather(A_path).to_numpy(dtype=np.float32)
     # Vector of edge weights
-    row_idx, col_idx = np.nonzero(A)
-    edge_weights = A[row_idx, col_idx]
+    row_idx, col_idx = np.nonzero(A) # who with whom
+    edge_weights = A[row_idx, col_idx] # edge weights 
     # Convert to torch tensors efficiently
     edge_index = torch.from_numpy(np.vstack([row_idx, col_idx]).astype(np.int64))
     edge_weights = torch.from_numpy(edge_weights)
+    #------------------------------
     # Section: Load target features 
-    tgt_path = os.path.join(tgt_dir, f"tgt_{id}.feather")
-    tgt_table =  feather.read_table(tgt_path, columns=['K_s'])
-    # columns_to_load = ['new_ext', 'BC_diss', 'K_s']  # all columns except K_s
-    #tgt_table =  feather.read_table(tgt_path, columns=columns_to_load)
+    tgt_path = os.path.join(targets_dir, f"ExtSummary_{id}.feather")
+    cols = ['prop_extinctions', 'dissimilarity_bc', 'keystoneness']
+    tgt_table =  feather.read_table(tgt_path, columns=cols)
     y_tensor = torch.from_numpy(tgt_table.to_pandas().to_numpy(dtype=np.float32))   
-    # Node features - simple ones vector
+    # Convert to 0 nan if any
+    y_tensor[torch.isnan(y_tensor)] = 0.0
+    #------------------------------ 
+    # Section: Load node features 
+    # Topological features
+    topology_path = os.path.join(topologies_dir, f"Topology_{id}.feather")
+    network_stats = feather.read_feather(topology_path)
+    network_stats = network_stats.drop(columns=["total_degree"])
+    x_tmp = torch.from_numpy(network_stats.to_numpy(dtype=np.float32))  
+    # Add dummy data, vector of ones
     n = A.shape[0]
-    x_tensor = torch.ones(n, 1, dtype=torch.float32)
+    dummy_tensor = torch.ones(n, 1, dtype=torch.float32)
+    x_tensor = torch.cat([x_tmp, dummy_tensor], dim=1)
+    #------------------------------
     # Clean up large intermediate
     del A, tgt_table
+    #------------------------------
     # Create Data object
     data = Data(
         x=x_tensor,
@@ -55,31 +73,35 @@ def load_single_data(id, A_dir, tgt_dir):
     )
     return data
 
-# Testing line
-id = data_ids.iloc[0]
-# load_single_data(data_ids.iloc[0], A_dir, tgt_dir)
-#===============================================
-# SECTION: Divide-data
+# Sanity check: load one data sample
+load_single_data(data_ids.iloc[0], interactions_dir, targets_dir, topologies_dir)
+
+#-------------------------------------------------------------
+# Section: Create batches of data
+# We create batches of data to speed up training.
+# Batches with more than 500 samples can cause memory issues, so we will use a batch size of 100.
 import random
 
 # Load all data samples (for demo, we use only first 100 samples)
 indices = list(range(1, len(data_ids)))  # Indices 1-100
 random.shuffle(indices)  # Uses Python's random module (already seeded)
 
-# Now select first 80 for training, rest for validation
-indx = round(len(indices) * .8)
-train_indices = indices[:indx]            # First 80 shuffled indices
-val_indices = indices[indx:]              # Last 20 shuffled indices
+# We will use all data for training and some for validation.
 
-#===============================================
-# Parallelize function
+# Now select first 80 for training, rest for validation
+# indx = round(len(indices) * .8)
+# train_indices = indices[:indx]            # First 80 shuffled indices
+# val_indices = indices[indx:]              # Last 20 shuffled indices
+
+#-------------------------------------------------------------
+# Section: Parallelize function
 from concurrent.futures import ThreadPoolExecutor
 import time
 from tqdm import tqdm
 
 # Generate data and separate in batches
-path = f'/mnt/data/sur/users/mrivera/Cuda-tensors/{os.path.basename(exp_dir)}'
-os.makedirs(path, exist_ok=True)
+tensors_path = f'/mnt/data/sur/users/mrivera/Cuda-tensors/{os.path.basename(experiment_dir)}'
+os.makedirs(tensors_path, exist_ok=True)
 
 def generate_data_parallel(idx, A_dir, tgt_dir, num_workers=4):  # idx is a list
     with ThreadPoolExecutor(max_workers=num_workers) as executor:
@@ -87,11 +109,13 @@ def generate_data_parallel(idx, A_dir, tgt_dir, num_workers=4):  # idx is a list
             load_single_data,           
             idx,                       # List of IDs to iterate over
             [A_dir]*len(idx),          # Repeat A_dir for each ID
-            [tgt_dir]*len(idx)         # Repeat tgt_dir for each ID
+            [tgt_dir]*len(idx),         # Repeat tgt_dir for each ID
+            [topologies_dir]*len(idx)  # Repeat topologies_dir for each ID
         ))
     return data_list
 
-
+#-------------------------------------------------------------
+# Section: Batching function
 def batching(all_ids, indexes, A_dir, tgt_dir, path, batch_size=250, num_workers=6, prefix='TrainBatch'):
     start = time.time()
     batching_ids = [all_ids[i] for i in indexes]
@@ -107,15 +131,19 @@ def batching(all_ids, indexes, A_dir, tgt_dir, path, batch_size=250, num_workers
     print(f">> The batch size is of {batch_size}, the number of batches was {num_batches}, while the elapsed time is of: {elapsed:.2f}")
     return None
 
-batching(all_ids = data_ids, indexes = train_indices, A_dir = A_dir, tgt_dir = tgt_dir, path = path, batch_size=100, num_workers=6, prefix='TrainBatch')
-batching(all_ids = data_ids, indexes = val_indices, A_dir = A_dir, tgt_dir = tgt_dir, path = path, batch_size=100, num_workers=6, prefix='ValBatch')
+batching(all_ids = data_ids, 
+         indexes = indices, A_dir = interactions_dir, 
+         tgt_dir = targets_dir, path = tensors_path, 
+         batch_size=100, num_workers=6, prefix='TrainBatch')
+
+# batching(all_ids = data_ids, indexes = val_indices, A_dir = A_dir, tgt_dir = tgt_dir, path = path, batch_size=100, num_workers=6, prefix='ValBatch')
 
 #===============================================
 # Create Zip of batches
 import shutil
 
 # Zip entire directory
-name = os.path.basename(exp_dir)
-shutil.make_archive(f'/mnt/data/sur/users/mrivera/Cuda-tensors/{name}', 'zip', 
-                    '/mnt/data/sur/users/mrivera/Cuda-tensors/exp_20251125')
+name = os.path.basename(experiment_dir)
+zip_dir = f'/mnt/data/sur/users/mrivera/Cuda-tensors/{name}'
+shutil.make_archive(zip_dir, 'zip', tensors_path)
 
