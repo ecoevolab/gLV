073dbe05b58cdb7410ac9648dd2b9ae0e1069d23
Rivera <rivera20c3@gmail.com>
2026-03-02 17:53:22 -0600

    updated function to print msg if no na is detected

--- fixers/keystoneness-fixing.r
+++ fixers/keystoneness-fixing.r
@@ -6,13 +6,18 @@
 train_dirs =  list.dirs('/mnt/data/sur/users/mrivera/Training-Data', recursive = FALSE)
 
 process_file <- function(f) {
-  arrow::read_feather(f) %>%
+  table <- arrow::read_feather(f) %>%
     mutate(
       keystoneness     = rel_pop_initial * dissimilarity_bc,
       prop_extinctions = n_extinctions / (n() - 1)
-    ) %>%
-    arrow::write_feather(sink = f)
-    cat('>> file', basename(f), ' completed...\n')
+    )
+  arrow::write_feather(x = table, sink = f)
+  if (!anyNA(table)) {
+    cat('>> file', basename(f), 'completed...\n')
+  } else {
+    cat('>> file', basename(f), 'has NAs!\n')
+  }
+  invisible(NULL)
 }
 
 all_files <- unlist(lapply(train_dirs, function(b) {
