f1e1021e219039e168eeba867d60ca7188eb438f
Rivera <rivera20c3@gmail.com>
2026-03-02 17:49:29 -0600

    generated fixers code to fix NA in keystoneness function

---
+++ fixers/keystoneness-fixing.r
@@ -0,0 +1,24 @@
+# 2-Mrach-2026
+# 
+# This code is for fixing the keyetoneness of some simulations were the index was wrong.
+
+# For training data
+train_dirs =  list.dirs('/mnt/data/sur/users/mrivera/Training-Data', recursive = FALSE)
+
+process_file <- function(f) {
+  arrow::read_feather(f) %>%
+    mutate(
+      keystoneness     = rel_pop_initial * dissimilarity_bc,
+      prop_extinctions = n_extinctions / (n() - 1)
+    ) %>%
+    arrow::write_feather(sink = f)
+    cat('>> file', basename(f), ' completed...\n')
+}
+
+all_files <- unlist(lapply(train_dirs, function(b) {
+  list.files(paste0(b, '/ExtSummaries'), full.names = TRUE)
+}))
+
+
+library(parallel)
+mclapply(all_files, process_file, mc.cores = parallel::detectCores() - 1)
