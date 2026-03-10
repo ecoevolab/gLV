6cd7a2bc8e965f035aa7d53e0abad4a2b4943aa3
Rivera <rivera20c3@gmail.com>
2026-03-10 00:46:28 -0600

    changed file name and fixed script to generate classes tensors

--- src-GNN/class-tensors.py
+++ src-GNN/class-tensors.py
@@ -13,7 +13,7 @@ import os
 from datetime import datetime
 
 # Section: Generate-paths
-experiment_dir = '/mnt/data/sur/users/mrivera/Controls/Cascade_keystone'
+experiment_dir = '/mnt/data/sur/users/mrivera/Controls/Boosted_keystone'
 interactions_dir = os.path.join(experiment_dir, "Interactions")
 targets_dir = os.path.join(experiment_dir, "ExtSummaries")
 topologies_dir = os.path.join(experiment_dir, "Topologies")
@@ -30,50 +30,59 @@ data = pd.read_csv(data_path, sep="\t", usecols=['id', 'key'])
 from torch_geometric.data import Data
 import pyarrow.feather as feather
 
-def load_single_data(row, interactions_dir, topologies_dir):
+def load_single_data(row, interactions_dir, targets_dir, topologies_dir):
     #------------------------------
     # Section: Load adjacency matrix 
     id = row['id']
     keystone = row['key'] - 1 # Convert to python 0 index
-    A_path = os.path.join(interactions_dir, f"A_{id}.feather")
-    A = pd.read_feather(A_path).to_numpy(dtype=np.float32)
-    # Vector of edge weights
-    row_idx, col_idx = np.nonzero(A) # who with whom
-    edge_weights = A[row_idx, col_idx] # edge weights 
-    # Convert to torch tensors efficiently
-    edge_index = torch.from_numpy(np.vstack([row_idx, col_idx]).astype(np.int64))
-    edge_weights = torch.from_numpy(edge_weights)
+    #-------------------------------
+    # Section: Generate target features
+    tgt_path = os.path.join(targets_dir, f"ExtSummary_{id}.feather")
+    rel_pops =  feather.read_table(tgt_path, columns=['rel_pop_initial']).to_pandas()
+    # Filter extinct species
+    alive_vector = rel_pops > 1e-06                 # vector
+    alive_indices = np.where(alive_vector)[0]           # indices
+    # Generate classes
+    n = len(rel_pops)
+    zero_tensor = torch.zeros(n,1) 
+    zero_tensor[keystone] = 1
+    y_tensor = zero_tensor[alive_indices]
     #------------------------------
-    # Section: Load target features 
-    n = A.shape[0]
-    y_tensor = torch.zeros(n,1) 
-    y_tensor[keystone] = 1 
+    # Section: Load adjacency matrix 
+    A_path = os.path.join(interactions_dir, f"A_{id}.feather")
+    A = feather.read_table(A_path).to_pandas()
+    # Subset interactions of alive population
+    A_subset = A.iloc[alive_indices, alive_indices].to_numpy()
+    # Vector of edge weights
+    row_idx, col_idx = np.nonzero(A_subset) # who with whom
+    edge_weights = A_subset[row_idx, col_idx] # edge weights 
+    # Convert to torch tensors efficiently
+    edge_index_tensor = torch.from_numpy(np.vstack([row_idx, col_idx]).astype(np.int64))
+    edge_weights_tensor = torch.from_numpy(edge_weights).float()   
     #------------------------------ 
     # Section: Load node features 
-    # Topological features
     topology_path = os.path.join(topologies_dir, f"Topology_{id}.feather")
-    network_stats = feather.read_feather(topology_path)
-    network_stats = network_stats.drop(columns=["total_degree"])
-    x_tmp = torch.from_numpy(network_stats.to_numpy(dtype=np.float32))  
+    network_stats = feather.read_feather(topology_path).iloc[alive_indices]
+    network_stats = network_stats.drop(columns=["total_degree"])                # Remove total degree
+    x_tensor = torch.from_numpy(network_stats.to_numpy(dtype=np.float32))       # Convert tensor
     # Add dummy data, vector of ones
-    dummy_tensor = torch.ones(n, 1, dtype=torch.float32)
-    x_tensor = torch.cat([x_tmp, dummy_tensor], dim=1)
+    n = x_tensor.shape[0]
+    x_tensor = torch.cat([x_tensor, torch.ones(n, 1, dtype=torch.float32)], dim=1)
     #------------------------------
     # Clean up large intermediate
-    del A
+    del A, A_subset
     #------------------------------
     # Create Data object
     data = Data(
         x=x_tensor,
-        edge_weights=edge_weights,
-        edge_index=edge_index,
+        edge_weights=edge_weights_tensor,
+        edge_index=edge_index_tensor,
         y=y_tensor
     )
     return data
 
 row = data.iloc[0]
-x = load_single_data(row, interactions_dir, topologies_dir)
-
+x = load_single_data(row, interactions_dir, targets_dir, topologies_dir)
 
 #-------------------------------------------------------------
 # Section: Parallelize function
@@ -81,16 +90,15 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from functools import partial
 from tqdm import tqdm  # optional, for progress bar
 
-def load_dataset_parallel(df, interactions_dir, topologies_dir, max_workers=8):
+def load_dataset_parallel(df, interactions_dir, targets_dir, topologies_dir, max_workers=8):
     # Fix arguments
-    func = partial(load_single_data, interactions_dir=interactions_dir, topologies_dir=topologies_dir)
+    func = partial(load_single_data, interactions_dir=interactions_dir, targets_dir=targets_dir, topologies_dir=topologies_dir)
     # Generate list of rows
     rows = [row for _, row in df.iterrows()]
     # Preallocate results
     results = [None] * len(rows)  
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         future_to_idx = {executor.submit(func, row): i for i, row in enumerate(rows)}
-        
         for future in tqdm(as_completed(future_to_idx), total=len(rows)):
             idx = future_to_idx[future]
             try:
@@ -100,11 +108,12 @@ def load_dataset_parallel(df, interactions_dir, topologies_dir, max_workers=8):
                 results[idx] = None
     return results
 
+
 #-------------------------------------------------------------
 # Section: Create batches
 import time
 
-def batching(df, A_dir, topology_dir, path, batch_size=250, num_workers=6, prefix='TrainBatch'):
+def batching(df, A_dir, targets_dir, topology_dir, path, batch_size=250, num_workers=6, prefix='TrainBatch'):
     #----------------------------
     # Section: Calculate batch size
     os.makedirs(path, exist_ok=True) # Generate directory if it does not exist
@@ -115,7 +124,7 @@ def batching(df, A_dir, topology_dir, path, batch_size=250, num_workers=6, prefi
     # Section: Process rows
     for i in tqdm(range(num_batches), desc="Processing batches"):
         batch_rows = df[i*batch_size : (i+1)*batch_size]  # slice the DataFrame
-        batch_data = load_dataset_parallel(batch_rows, A_dir, topology_dir, num_workers)  # different name
+        batch_data = load_dataset_parallel(batch_rows, A_dir, targets_dir, topology_dir, num_workers)  # different name
         name = os.path.join(path, f'{prefix}_{i}.pt')
         torch.save(batch_data, name)
         del batch_data  # free memory
@@ -125,7 +134,7 @@ def batching(df, A_dir, topology_dir, path, batch_size=250, num_workers=6, prefi
     remainder = rows % batch_size
     if remainder > 0:
         batch_rows = df[num_batches*batch_size:]
-        batch_data = load_dataset_parallel(batch_rows, A_dir, topology_dir, num_workers)
+        batch_data = load_dataset_parallel(batch_rows, A_dir, targets_dir, topology_dir, num_workers)
         name = os.path.join(path, f'{prefix}_{num_batches}.pt')
         torch.save(batch_data, name)
         del batch_data
@@ -136,18 +145,22 @@ def batching(df, A_dir, topology_dir, path, batch_size=250, num_workers=6, prefi
 
 #-----------------------
 # Run parallelization
-tensors_path = '/mnt/data/sur/users/mrivera/Cuda-tensors/Cascade_classes'
+tensors_path = '/mnt/data/sur/users/mrivera/Cuda-tensors/Boosted_filter-classes'
 batching(df = data,
          A_dir = interactions_dir, 
+         targets_dir = targets_dir,
          topology_dir = topologies_dir, 
          path = tensors_path, 
-         batch_size=100, num_workers=6, prefix='TrainBatch_')
+         batch_size=100, num_workers=6, prefix='TrainBatch')
+
+
 
 #===============================================
 # Create Zip of batches
 import shutil
 
 # Zip entire directory
-name = os.path.basename(experiment_dir)
+name = os.path.basename(tensors_path)
 zip_dir = f'/mnt/data/sur/users/mrivera/Cuda-tensors/{name}'
 shutil.make_archive(zip_dir, 'zip', tensors_path)
+
