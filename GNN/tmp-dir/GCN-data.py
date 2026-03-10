cc916122304078cb171cbc790649af313f64f6b2
Rivera <rivera20c3@gmail.com>
2026-03-10 00:09:30 -0600

    removing old GNN directory

--- GNN/tmp-dir/GCN-data.py
+++
@@ -1,7 +0,0 @@
-# Developing Graph Attention Network (GAT)
-import torch
-import torch.nn as nn
-from torch_geometric.nn import GATConv
-from torch_geometric.data import Data
-import torch.nn.functional as F
-import torch.optim as optim
